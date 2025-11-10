3ef3f672d535b963be5fd38b46c9edb44de7d9a3
Matthias <teamnest2013@gmail.com>
2025-11-10 20:06:24 +0100

    Initialer Commit – Grundgerüst Shoot Like Rogue

---
+++ src/playersData.ts
@@ -0,0 +1,7 @@
+export const playersData = [
+  { name: "Luca Ferro", role: "Striker", shot: 80, pass: 60, defend: 30 },
+  { name: "Kai Moreno", role: "Midfielder", shot: 65, pass: 78, defend: 55 },
+  { name: "Ryo Tanaka", role: "Defender", shot: 45, pass: 55, defend: 85 },
+  { name: "Noah Glint", role: "Goalie", shot: 20, pass: 60, defend: 90 },
+];
+
